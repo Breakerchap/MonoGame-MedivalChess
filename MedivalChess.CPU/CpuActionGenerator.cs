83817e11aac344eff6e63508cffdf378f42fd598
Remy E <remyscalendar@gmail.com>
2026-09-22 07:55:33 +1000

    Generate CPU attachment ability actions

--- MedivalChess.CPU/CpuActionGenerator.cs
+++ MedivalChess.CPU/CpuActionGenerator.cs
@@ -324,6 +324,18 @@ public sealed class CpuActionGenerator : ICpuActionGenerator
         }
       }
     }
+    else if (actor.Type is nameof(PieceType.Muse) or nameof(PieceType.Shieldsman) or nameof(PieceType.Imp))
+    {
+      foreach (NetworkPiece target in state.Pieces.Where(piece => piece.Team == actor.Team && piece.Id != actor.Id)
+        .OrderBy(piece => piece.Id, StringComparer.Ordinal))
+      {
+        foreach ((int x, int y) targetSquare in GetTargetSquares(target))
+        {
+          AddIfLegal(state, new UseAbilityAction(
+            actor.Team, actor.Id, "Attach", target.Id, targetSquare.x, targetSquare.y), actions);
+        }
+      }
+    }
     else if (actor.Type is "Guard" or "Ox")
     {
       foreach (NetworkPiece target in state.Pieces.Where(piece => piece.Team == actor.Team && piece.Id != actor.Id)
