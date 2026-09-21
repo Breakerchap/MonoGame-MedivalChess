ce4328b38b754e6194189d085e58eb38192e5f20
Remy E <remyscalendar@gmail.com>
2026-09-22 07:17:28 +1000

    Fix local terrain effects imports

--- Game1.AbilityEntityEffects.cs
+++ Game1.AbilityEntityEffects.cs
@@ -1,4 +1,8 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using MedivalChess.GameBoard;
+using MedivalChess.Player;
 using MedivalChess.Shared;
 
 namespace MedivalChess;
